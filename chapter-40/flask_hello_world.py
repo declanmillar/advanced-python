41ad25a00460d702deb9fd95c41514239407e953
Declan Millar <declan.millar@ibm.com>
2020-05-19 21:01:49 +0100

    Finish chapter 40

---
+++ chapter-40/flask_hello_world.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def welcome():
+    return jsonify({'msg': 'Hello Flask World'})
+
+app.run(debug=True)
